9503e5344ee6ec329dd0f28cf7cb2147448b3c32
Alex Jackson <alex@alexj.org>
2015-08-16 10:51:23 +0100

    Initial Commit

---
+++ gyoutube-dl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gyoutube-dl
+//
+//  Created by Alex Jackson on 16/08/2015.
+//  Copyright © 2015 Alex Jackson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gyoutube-dl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gyoutube-dl
+//
+//  Created by Alex Jackson on 16/08/2015.
+//  Copyright © 2015 Alex Jackson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

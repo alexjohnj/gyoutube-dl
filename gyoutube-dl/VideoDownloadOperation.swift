afe9d8a86e22702f1567d488f9cfbbec8ab2270b
Alex Jackson <alex@alexj.org>
2015-08-16 12:43:48 +0100

    Create VideoDownloadOperation

---
+++ gyoutube-dl/VideoDownloadOperation.swift
@@ -0,0 +1,13 @@
+//
+//  VideoDownloadOperation.swift
+//  gyoutube-dl
+//
+//  Created by Alex Jackson on 16/08/2015.
+//  Copyright © 2015 Alex Jackson. All rights reserved.
+//
+
+import Cocoa
+
+class VideoDownloadOperation: NSOperation {
+
+}

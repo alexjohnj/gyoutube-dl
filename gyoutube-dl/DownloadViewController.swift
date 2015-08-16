17952acc08527db5dd6d6bb78dc30526c37e1a60
Alex Jackson <alex@alexj.org>
2015-08-16 12:43:02 +0100

    Add Download View sheet

---
+++ gyoutube-dl/DownloadViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadViewController.swift
+//  gyoutube-dl
+//
+//  Created by Alex Jackson on 16/08/2015.
+//  Copyright © 2015 Alex Jackson. All rights reserved.
+//
+
+import Cocoa
+
+class DownloadViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

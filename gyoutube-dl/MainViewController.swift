d13077969bcad283f872706c15a69b40283980ea
Alex Jackson <alex@alexj.org>
2015-08-16 11:56:08 +0100

    Implement basic table view data source methods

--- gyoutube-dl/MainViewController.swift
+++ gyoutube-dl/MainViewController.swift
@@ -8,32 +8,51 @@
 
 import Cocoa
 
-class MainViewController: NSViewController {
-
+class MainViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+    // MARK: Properties
+    var videoLinks: [NSURL] = []
+    
+    // MARK: Outlets
     @IBOutlet weak var videoTable: NSTableView!
     @IBOutlet weak var urlField: NSTextField!
     @IBOutlet weak var addButton: NSButton!
     @IBOutlet weak var removeButton: NSButton!
     @IBOutlet weak var startButton: NSButton!
     
+    // MARK: NSViewController Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
+    
+    // MARK: Actions
     @IBAction func addLink(sender: AnyObject) {
     }
     @IBAction func removeSelectedLink(sender: AnyObject) {
     }
     @IBAction func startDownload(sender: AnyObject) {
+    }
+    
+    // MARK: TableView Methods
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return videoLinks.count
+    }
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject?  {
+        return videoLinks[row]
+    }
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let tableColumn = tableColumn else {
+            print("No table column identifier")
+            return nil
+        }
+        let cellView = tableView.makeViewWithIdentifier(tableColumn.identifier, owner: self) as! NSTableCellView
+        cellView.objectValue = videoLinks[row]
+        cellView.textField?.stringValue = videoLinks[row].absoluteString
+        
+        return cellView
     }
 }
 
